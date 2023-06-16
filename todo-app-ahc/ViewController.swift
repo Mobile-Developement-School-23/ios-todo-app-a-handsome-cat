e552afdd0fb671b81a2f1679ebc32b3e8abf5514
a-handsome-cat <shol-arh@yandex.ru>
2023-06-16 21:39:13 +0200

    Initial Commit

---
+++ todo-app-ahc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todo-app-ahc
+//
+//  Created by Артемий Москалев on 16.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
